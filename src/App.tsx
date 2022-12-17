99c849a14ad6c4600ff3caee0e751643afad6d1a
Joshua Doan <insaddingaround@gmail.com>
2022-12-17 10:42:11 -0800

    dzsf

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import Header from "./components/Header";
 import useGame from "./hooks/useGame";
-
 import "./App.css";
-import SideBar from "./components/SideBar";
-import { State, Event } from "./types";
 import { Link } from "react-router-dom";
 import { isShip, isStation } from "./utils";
-import { ArrowLeftCircleIcon } from "@heroicons/react/24/outline";
 
 function App() {
   const { state, dispatch } = useGame();
@@ -100,24 +95,6 @@ function App() {
 }
 
 export default App;
-
-function MenuIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      className="inline-block w-5 h-5 stroke-current"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-      ></path>
-    </svg>
-  );
-}
 
 function HamburgerIcon() {
   return (
